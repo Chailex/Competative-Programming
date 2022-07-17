bba71294e46570ac09993f5bccabf62644bde7dd
chailex <chailexsarma@gmail.com>
2022-07-17 23:50:46 +0530

    24th commit

--- Dynamic_Programming_fibonacci.cpp
+++ Dynamic_Programming_fibonacci.cpp
@@ -1,20 +1,51 @@
+//TOP DOWN approach
+// #include<bits/stdc++.h>
+// using namespace std;
+// const int N = 1e5+10;
+// int dp[N];
+// //Top down approach
+
+// int fib(int n){
+// 	if(n == 0) return 0;
+// 	if(n == 1) return 1;
+// 	if(dp[n] != -1) return dp[n];
+// 	//memoise
+
+// 	return dp[n] = fib(n-1)+fib(n-2);
+// }
+
+// int main(){
+// 	memset(dp, -1, sizeof(dp));
+// 	int n;
+// 	cin>>n;
+// 	cout<<fib(n);
+// 	return 0;
+// }
+
+//Bottom Up approach
+
 #include<bits/stdc++.h>
 using namespace std;
 const int N = 1e5+10;
 int dp[N];
+//Bottom up approach
 
 int fib(int n){
-	if(n == 0) return 0;
-	if(n == 1) return 1;
+	if(n == 0) return dp[0];
+	if(n == 1) return dp[1];
 	if(dp[n] != -1) return dp[n];
-
-	return dp[n] = fib(n-1)+fib(n-2);
 }
 
 int main(){
 	memset(dp, -1, sizeof(dp));
 	int n;
-	cin>>n;
+	cin>>n; 
+	//Bottom up approach
+	dp[0] = 0;
+	dp[1] = 1;
+	for(int i=2; i<= n; ++i){
+		dp[i] = dp[i-1]+dp[i-2];
+	}
 	cout<<fib(n);
 	return 0;
 }
